0e5dd0956fc1e37aa101285049929a085b7f2547
LizzardKing1967 <127925968+LizzardKing1967@users.noreply.github.com>
2023-09-07 19:07:34 +0300

    MAIN1

--- src/Main/Runner.java
+++ src/Main/Runner.java
@@ -2,6 +2,6 @@ package Main;
 
 public class Runner {
     public static void main (String[] args) {
-        System.out.println("Start_main");
+        System.out.println("Start_mainЙ2313");
     }
 }
