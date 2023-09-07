5c147d2459519234cb2114ad87504076bc6feba2
LizzardKing1967 <127925968+LizzardKing1967@users.noreply.github.com>
2023-09-07 18:49:14 +0300

    ФЫС

--- src/Main/Runner.java
+++ src/Main/Runner.java
@@ -2,6 +2,6 @@ package Main;
 
 public class Runner {
     public static void main (String[] args) {
-        System.out.println("Start_First_1");
+        System.out.println("Start_First_1ЧЧЯМЧМЧМ");
     }
 }
